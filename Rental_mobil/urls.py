80acbfd1c4c516f77c0d18e40efc549a3844e572
Rahman Hakim <rahmanhakim2435@gmail.com>
2020-10-16 09:59:47 +0700

    Menambah kontak dan about

--- Rental_mobil/urls.py
+++ Rental_mobil/urls.py
@@ -3,13 +3,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import index_view
+from .views import index_view, about_view, contact_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_view, name="home"),
     path('rent/', include('sewa_mobil.urls')),
     path('accounts/', include('akun.urls')),
+    path('about/', about_view, name="about"),
+    path('contact/', contact_view, name="contact"),
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Rental_mobil/views.py
+++ Rental_mobil/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, 'main/index.html')
+
+def contact_view(request):
+    return render(request, 'main/contact.html')
+
+def about_view(request):
+    return render(request, 'main/about.html')
+
+
